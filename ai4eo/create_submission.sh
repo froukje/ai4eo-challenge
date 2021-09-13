de27a298cb3aa768da3db24b8261675e4e0e5389
Caroline Arnold <arnold@dkrz.de>
2021-09-13 14:12:03 +0200

    Updated create submission script

--- ai4eo/create_submission.sh
+++ ai4eo/create_submission.sh
@@ -3,6 +3,6 @@
 # takes the submission directory as an argument
 # creates the submission TIFFs
 # zips the folder
-python submission.py --raw-data-dir /swork/shared_data/2021-ai4eo/eopatches/ --target-dir $1  --trained-model /home/dkrz/k202143/nni-experiments/pZY9sCrI/trials/LDqN2/best_model.pt --bands B02 B03 B04 B05 B06 B07 B08 B8A B11 B12 --max-epochs 500 --learning-rate 1e-4 --patience 50 --n-blocks 49 --n-channels 125 --n-time-frames 6
-tar -C $1 -zcvf submission_pZY9sCrI_LDqN2.tar.gz . 
+python submission.py --raw-data-dir /swork/shared_data/2021-ai4eo/eopatches/ --target-dir $1  --trained-model /swork/caroline/jobs/ai4eo/all_indices_random/epoch_120_model.pt --bands B02 B03 B04 B08 --n-blocks 52 --n-channels 117 --n-time-frames 6 
+tar -C $1 -zcvf $1.tar.gz . 
 #tar -zcvf submission.tar.gz . 
